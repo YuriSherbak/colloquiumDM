a1ce280682514824f6f421b9d211a8449630e523
Yuri Sherbak <uraserbak363@gmail.com>
2020-03-28 18:05:10 +0300

    Добавлена функция считывания строки в натруальных числах

--- colloquiumDM/Parser.cpp
+++ colloquiumDM/Parser.cpp
@@ -1,12 +1,38 @@
 #include "Parser.h"
 
-N ReadNumber_N(string str)
-{
-	vector <short int> num;
-	for (int i = 0; i < str.size(); i++)
-		num.push_back((int)str[i] - 48);
+enum mathOp {plus = '+', minus = '-', mul = '*', diiv = '//'};
 
+N ReadNumber_N(string str, int from, int to)
+{
+	mathOp sign;
+	vector <short int> num;
+	for(int i = from; i < to; i++)
+	{
+		num.push_back((int)str[i] - 48);
+		i++;
+	}
 	return N(num);
+}
+
+int SearchSign(string str)
+{
+	mathOp sign;
+	for (int i = 0; i < str.size(); i++)
+	{
+		if (str[i] == sign)
+			return i;
+	}
+	return -1;
+}
+
+N ParseStr_N(string str)
+{
+	str.pop_back();
+	int positionSign = SearchSign(str);
+	N num1 = ReadNumber_N(str, 0, positionSign);
+	N num2 = ReadNumber_N(str, positionSign + 1, str.size());
+	char sign = str[positionSign];
+	return N();
 }
 
 Z ReadNumber_Z(string str)
